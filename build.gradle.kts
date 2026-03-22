efed66be9033df6ca9554a41f7fee6ce8e6d7b32
Oissat <tassiolucas@live.com>
2026-03-22 02:17:38 -0300

    Adiciona versão compatível do gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ val logback_version: String by project
 val exposed_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.22"
-    id("io.ktor.plugin") version "2.3.8"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+    kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "3.1.1"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
     application
 }
 
